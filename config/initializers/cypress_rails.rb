5901e1a7d303ef49f06dcf6bba16b0e56a0ab639
max k <maxkuhn@gmail.com>
2022-08-30 22:07:57 -0700

    Add cypress_rails.rb and home.spec.js

--- config/initializers/cypress_rails.rb
+++ config/initializers/cypress_rails.rb
@@ -8,12 +8,21 @@ CypressRails.hooks.before_server_start do
   cat1 = Category.find_or_create_by! name: 'Evergreens'
 
   cat1.products.create!({
-    name: 'Amorphophallus Titanum',
-    description: 'Amorphophallus Titanum, also known as the "Corpse Flower," is a ten-foot flower that smells like dead bodies and blooms every 40 years.',
-    image: open_asset()
+    name:  'Giant Tea',
+    description: "The Giant Tea is an uncommon, medium-sized plant and can be found only in some tundras. It blooms twice a year, for 3 weeks.",
+    image: open_asset('plante_1.jpg'),
+    quantity: 0,
+    price: 64.99
   })
 
-
+  cat1.products.create!({
+    name:  'Scented Blade',
+    description: "
+    The Scented Blade is an extremely rare, tall plant and can be found mostly in savannas. It blooms once a year, for 2 weeks.",
+    image: open_asset('plante_2.jpg'),
+    quantity: 18,
+    price: 24.99
+  })
 end
 
 CypressRails.hooks.after_transaction_start do
@@ -26,4 +35,11 @@ end
 
 CypressRails.hooks.before_server_stop do
   # Called once, at_exit
+  DatabaseCleaner.clean
+end
+
+private
+
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
 end
