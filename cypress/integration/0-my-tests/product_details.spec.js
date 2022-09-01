70c57176ae39481529898ab8200bc75b06df35b5
max k <maxkuhn@gmail.com>
2022-08-31 17:23:15 -0700

    Add successful product testing

---
+++ cypress/integration/0-my-tests/product_details.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('Can see the logo', () => {
+    cy.get('.navbar-brand').should('have.text', 'Jungle');
+  });
+
+  it("Can visit the right product detail page", () => {
+    cy.get(".products article").first().click();
+    cy.get(".product-detail").should("be.visible");
+    cy.get(".product-detail h1").should("have.text", "Scented Blade");
+  });
+
+
+});
